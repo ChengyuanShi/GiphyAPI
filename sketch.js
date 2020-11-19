6e2f9bf0aca23c01d438aac82a096a4f3c364143
chengyuanshi <chengyuan.shi@sjsu.edu>
2020-11-18 20:42:46 -0800

    1

---
+++ sketch.js
@@ -0,0 +1,27 @@
+
+var api = "http://api.giphy.com/v1/gifs/search?";
+var apiKey = "&api_key=eaw3IYGqk5f4nex5DCbpZjZdR8JxhSPK";
+var query = "&q=JOJO";
+
+
+
+
+function setup() {
+  noCanvas();
+  var url = api + apiKey + query;
+  loadJSON(url, gotData);
+
+
+}
+
+function gotData(giphy){
+  for (var i = 0; i< giphy.data.length; i++){
+
+  createImg(giphy.data[i].images.original.url);
+
+
+}
+}
+function draw() {
+
+}
