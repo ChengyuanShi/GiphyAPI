a695db14f458761305af8cbe8f9a019af2f6c608
chengyuanshi <chengyuan.shi@sjsu.edu>
2020-11-18 21:14:52 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,27 @@
 
+var api = "http://api.giphy.com/v1/gifs/search?";
+var apiKey = "&api_key=eaw3IYGqk5f4nex5DCbpZjZdR8JxhSPK";
+var query = "&q=anime";
 
 
 
 
 function setup() {
-background(0,100,0)
+  noCanvas();
+  var url = api + apiKey + query;
+  loadJSON(url, gotData);
+
 
 }
 
+function gotData(giphy){
+  for (var i = 0; i < giphy.data.length; i++){
 
+  createImg(giphy.data[i].images.original.url);
+
+
+}
+}
 function draw() {
 
 }
